d2672aae746c32234d518401537b5a55828fc455
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-01-22 00:09:01 -0300

    First Commit

---
+++ Aulas PY/functions.js
@@ -0,0 +1,94 @@
+$(function () {
+
+    $('.aula1 h4').click(function () {
+        if ($('.aula1 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula1 .hidden').css('display', 'block');
+            $('.aula1 span').html('v');
+        }
+        else {
+            $('.aula1 .hidden').css('display', 'none');
+            $('.aula1 span').html('>');
+        }
+    });
+
+    $('.aula2 h4').click(function () {
+        if ($('.aula2 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula2 .hidden').css('display', 'block');
+            $('.aula2 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+    $('.aula3 h4').click(function () {
+        if ($('.aula3 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula3 .hidden').css('display', 'block');
+            $('.aula3 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+    $('.aula4 h4').click(function () {
+        if ($('.aula4 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula4 .hidden').css('display', 'block');
+            $('.aula4 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+    $('.aula5 h4').click(function () {
+        if ($('.aula5 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula5 .hidden').css('display', 'block');
+            $('.aula5 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+    $('.aula6 h4').click(function () {
+        if ($('.aula6 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula6 .hidden').css('display', 'block');
+            $('.aula6 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+    $('.aula7 h4').click(function () {
+        if ($('.aula7 .hidden').css('display') == 'none') {
+            $('.aula .hidden').css('display', 'none');
+            $('.aula span').html('>');
+            $('.aula7 .hidden').css('display', 'block');
+            $('.aula7 span').html('v');
+        }
+        else {
+            $('.hidden').css('display', 'none');
+            $('span').html('>');
+        }
+    });
+
+})
